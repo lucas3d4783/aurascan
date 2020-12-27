083f3a91839268f47c2ab46e27ba7405d03b2c25
lucas3d4783 <lucas3d4783@gmail.com>
2020-12-26 23:48:17 -0300

    Criando e estilizando a Home do Site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="{{ asset('img/icons/logo.png') }}" type="image/png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> 
             @if (isset($title)) 
@@ -13,10 +14,31 @@
     </head>
     <body>
         <div class="nav">
-            Teste
+            <div class="container-start-nav">
+                <a href="#" class="btn-nav boder-purple">
+                    <img src="{{ asset('img/icons/logo.png') }}" alt="Aura Scan"> Aura Scan
+                </a>
+            </div>
+            
+            
+            <div class="container-end-nav">
+                <a href="#" class="btn-nav">Sobre</a>
+                <a href="#" class="btn-nav">Contato</a>
+            </div>
+            
         </div>
+
+        <header>
+            Bem-cindo
+        </header>
+        
         <div class="main">
             <h1>Aura Scan</h1>
         </div>
+
+        <footer>
+            Feito por Lucas Coelho Reichert
+        </footer>
+
     </body>
 </html>
